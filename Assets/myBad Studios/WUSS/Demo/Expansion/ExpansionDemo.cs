d0e2bcaef36e9855662deb3687407467aa75960f
JansenSensei <mybadstudios@gmail.com>
2020-07-19 17:42:21 +0200

    Initial commit INSTALL WULOGIN OR WORDPRESS FOR UNTIY TO GET RID OF THE ERRORS WHEN YOU OPEN THIS PROJECT

---
+++ Assets/myBad Studios/WUSS/Demo/Expansion/ExpansionDemo.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using MBS;
+
+public class ExpansionDemo : MonoBehaviour {
+    void Start() => WUExpansion.OnSubscriptionTested += DisplayVerdict;
+
+    void DisplayVerdict() => Debug.Log($"User {WULogin.username} does{ (WUExpansion.has_subsciption ? "" : " NOT") } have a subsciption" );
+}
---
+++ Assets/myBad Studios/WUSS/Scripts/Custom/WUExpansion.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace MBS
+{
+    public class WUExpansion : MonoBehaviour
+    {
+        enum WUExpActions { IsSubscribed }
+        const string filepath = "wuss_expansion/unity_functions.php";
+        const string ASSET = "CUSTOM";
+
+        static public bool has_subsciption = false;
+        static public System.Action OnSubscriptionTested;
+
+        void Start() => WULogin.onLoggedIn += FetchSubscription;
+        void FetchSubscription( CML ignore ) => WPServer.ContactServer( WUExpActions.IsSubscribed, filepath, ASSET, null, TestResponse, ( CMLData result ) => Debug.LogWarning( result.String( "message" ) ) ); //in 2018.1 change CMLData to CMLData
+
+        void TestResponse( CML response )
+        {
+            has_subsciption = response [0].Bool( "subscribed" );
+            OnSubscriptionTested?.Invoke();
+        }
+    }
+}
